6622b7fe2d37c0c81c9f0c3bb94acd4d7a2fc6aa
Siri <sdandu99@gmail.com>
2020-07-14 10:28:31 -0400

    Initial Commit

---
+++ howToQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  howToQuiz
+//
+//  Created by Siri Dandu on 7/14/20.
+//  Copyright © 2020 Siri Dandu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
